a71f6f166bdf3ab923609d8ba7d5186181118be6
dpgaspar <danielvazgaspar@gmail.com>
2015-10-26 13:22:08 +0000

    included translations (pt) as an example

--- app/views.py
+++ app/views.py
@@ -1,3 +1,4 @@
+from flask import render_template
 from flask.ext.appbuilder import ModelView
 from flask.ext.appbuilder.models.mongoengine.interface import MongoEngineInterface
 from app import appbuilder
@@ -5,4 +6,12 @@ from app import appbuilder
 """
     Define you Views here
 """
+
+
+"""
+    Application wide 404 error handler
+"""
+@appbuilder.app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html', base_template=appbuilder.base_template, appbuilder=appbuilder), 404
 
